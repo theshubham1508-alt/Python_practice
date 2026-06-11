d4b567e66ac8e2113929e312816aefee25c0c51b
Shubham Kumar <theshubham1508@gmail.com>
2026-06-11 22:04:18 +0530

    first commit

---
+++ OneDrive/Firstcode.py/Firstcode.py
@@ -0,0 +1,34 @@
+#logical  operator
+a = 50
+b = 30
+print(not (a>b))
+print( not False)
+
+
+val1=False
+val2=True
+print("AND operator:",val1 and val2)
+
+val1=False
+val2=True
+print("OR operator:",val1 or val2)
+
+
+#type coversion
+a = 2
+b = 4.25
+print(a + b)
+
+#type casting
+a = int("2")
+b = 4.25
+print(type(a))
+print(a + b)
+
+#input
+a = int(input("enter first number:"))
+b = int(input("enter second number:"))
+
+print(a>=b)
+
+
